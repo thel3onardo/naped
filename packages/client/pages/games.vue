25bea9eb7e57dde4c8ade960776944e827ca51d7
thel3onardo <leonardo.contato03@gmail.com>
2022-02-28 16:47:13 -0300

    feat(client): Creates games page

---
+++ packages/client/pages/games.vue
@@ -0,0 +1,13 @@
+<template>
+  <CategoryIndex
+    background-path="images/games/capa-noticia.jpg"
+    category="games"
+    category-description="Lorem ipsum meu deus do ceu"
+  />
+</template>
+
+<script>
+export default {
+  name: 'Games'
+}
+</script>
