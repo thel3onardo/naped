20a4aee6bc2f88c84565df641758458e7c5df555
thel3onardo <leonardo.contato03@gmail.com>
2022-02-28 12:11:20 -0300

    fix(client): Aligns navbar items vertically

---
+++ packages/client/pages/filmes.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="w-100">
+    <CategoryHeader
+      background-path="images/filmes/capa-noticia.png"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis ipsum facilis ad est deleniti."
+      category="filmes"
+      @changePostsList="changePosts"
+    />
+    <div class="row py-5">
+      <div v-for="post in posts" :key="post.id" class="col-12 col-md-6">
+        <post-card
+          :title="post.title"
+          :category="post.category"
+          :background-img-url="post.image_url"
+          :category-visible="false"
+          :title-font-size="0.9"
+          height="250px"
+          justify-content-type="justify-content-end"
+          class="my-4"
+        />
+        <div />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PostCard from '../components/PostCard.vue'
+
+export default {
+  name: 'Filmes',
+  components: { PostCard },
+  data () {
+    return {
+      posts: []
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
